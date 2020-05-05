946a884b1fc3233a0cf98bdea25ec6201ef1f1f4
WQTKOD57 <55468492+WQTKOD57@users.noreply.github.com>
2020-05-04 20:54:25 -0400

    upload plots

--- code/stdp.py
+++ code/stdp.py
@@ -5,7 +5,7 @@ from network import supervised
 from xorBCM import getDataset
 from utility import plotSpike
 import random
-
+from utility import plotSpikeList
 
 def getNetwork(fMin = 50, fMax = 250, capitance = 1, resistance = 1, vThreshold = 0.7, vRest = 0, tau = 20, minSupervisedCurrent =
                -1, maxSupervisedCurrent = 1, learningRate = 1, dt = 1e-3):
@@ -68,24 +68,24 @@ def train(SNN, dataX, dataY, iterNum, stdp_config, forwardTime = 1000, refreshTi
     return
 
 
-def test(SNN, dataX, n_iter_test = 10, forwardTime = 1):
+def test(SNN, dataX, iterNum, forwardTime = 1000, plot = False, legend = True, fn_save = None):
     dataSize = dataX.shape[0]
     idxList = np.array(range(dataSize), dtype = np.int8)
     hit = 0
-    testResult = [[None for i in range(dataSize)] for j in range(n_iter_test)]
-    for j in range(n_iter_test):
+    testResult = [[None for i in range(dataSize)] for j in range(iterNum)]
+    for j in range(iterNum):
         np.random.shuffle(idxList)
         for idx in idxList:
             SNN.reset()
             print(' %d, %d: ' %(dataX[idx, 0].astype(np.int8), dataX[idx, 1].astype(np.int8)), end = '')
             spike = SNN.batchedPredict(dataX[idx], forwardTime)
             print('%.2f' %(np.sum(spike).astype(np.float16)))
-            rate = np.sum(spike, axis = 0).astype(np.float32) / forwardTime * 1000
+            rate = np.sum(spike, axis = 0).astype(np.float32) / forwardTime * 1
             print(rate)
-            testResult[iters][idx] = rate
+            testResult[j][idx] = rate
             if plot is True:
-                plotSpikeList(SNN.spikeListList, legend = legend, fn_save = fn_save + '.input%d.iter%d' %(idx, iters))
-    print('Accuracy: %.2f%%' %(100.0 * hit / n_iter_test / 4))
+                spikeListList = [SNN.spikeListList[0], SNN.spikeListList[-1]]
+                plotSpikeList(spikeListList, legend = legend, fn_save = fn_save + '.input%d.iter%d' %(idx, j))
     return
 
 
@@ -114,4 +114,4 @@ if __name__ == "__main__":
     train(SNN, dataX, dataY, 30, stdp_config, forwardTime=2, refreshTime=1)
     SNN._printWeight()
     print('---- TESTING ----')
-    test(SNN, dataX, forwardTime=2)
+    SNN = test(SNN, dataX, iterNum = 10, forwardTime = 2, plot = True, fn_save = 'stdp')
